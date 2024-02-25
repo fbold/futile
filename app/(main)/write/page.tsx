c38b726c45015e024aca7a511d07c2f87538d9ff
Vurak <vurak.of.sulcan@gmail.com>
2024-02-25 15:17:58 +0100

    hooking up the editor to new endpoint to create "tiles" (generic term for thoughts, writings, poems, etc)

--- app/(main)/write/page.tsx
+++ app/(main)/write/page.tsx
@@ -1,11 +1,28 @@
 "use client"
 import Editor from "@/components/editor"
+import { useRouter } from "next/navigation"
 
 export default function Write() {
+  const router = useRouter()
+  const handleSave = async (title: string, content?: string) => {
+    const post = await fetch("/api/tile", {
+      method: "POST",
+      body: JSON.stringify({
+        title,
+        content,
+        category: "thought",
+      }),
+    })
+
+    if (post.ok && post.status === 200) {
+      router.push("/")
+    }
+  }
+
   return (
     <div className="flex justify-center h-full max-h-full scroll">
       <div className="relative bg-pri dark:bg-pri-d md:px-8 md:py-4 flex flex-col w-full md:w-2/3 lg:w-1/2 h-full min-h-full">
-        <Editor onSave={(e) => ({})} saveResult={true} />
+        <Editor onSave={handleSave} saveResult={true} />
       </div>
     </div>
   )

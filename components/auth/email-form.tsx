40bfc5f1d0e28d7e288b4bcca8ac87b7a5fbb926
Vurak <vurak.of.sulcan@gmail.com>
2024-05-10 11:30:08 +0200

    get zod resolver wokring with form for username and password (twicE)

--- components/auth/email-form.tsx
+++ components/auth/email-form.tsx
@@ -4,15 +4,27 @@ import { z } from "zod"
 // import { TextInput } from "@/components/core/text-input"
 // import Button from "@/components/core/button"
 import { SubmitHandler, useForm } from "react-hook-form"
+import { zodResolver } from "@hookform/resolvers/zod"
 import { useState } from "react"
 import { redirect } from "next/navigation"
 import { TextInput } from "@/components/input"
 import { DefaultButton } from "@/components/buttons"
 
-const LoginSchema = z.object({
-  email: z.string().email({ message: "Must be a valid email" }),
-  name: z.string().min(2, "Must be at least 2 characters"),
-})
+const LoginSchema = z
+  .object({
+    username: z.string().min(4, "Must be at least 4 characters"),
+    password: z.string().min(10, "Must be at least 10 characters"),
+    confirmPassword: z.string().min(10, "Must be at least 10 characters"),
+  })
+  .superRefine(({ confirmPassword, password }, ctx) => {
+    if (confirmPassword !== password) {
+      ctx.addIssue({
+        path: ["confirmPassword"],
+        code: "custom",
+        message: "Passwords don't match",
+      })
+    }
+  })
 
 type LoginType = z.infer<typeof LoginSchema>
 
@@ -24,32 +36,37 @@ export default function EmailSignOn() {
     handleSubmit: handleLogin,
     formState: { errors },
   } = useForm<LoginType>({
-    defaultValues: { email: "fred@fred.com" },
+    defaultValues: { username: "frodo" },
+    resolver: zodResolver(LoginSchema),
   })
 
-  const onLogin: SubmitHandler<LoginType> = async (data) => {
-    if (!needsRegister) {
-      const signInAttempt = await signIn("email", {
-        email: data.email,
-        redirect: false,
-      })
+  // const onLogin: SubmitHandler<LoginType> = async (data) => {
+  //   if (!needsRegister) {
+  //     const signInAttempt = await signIn("email", {
+  //       email: data.email,
+  //       redirect: false,
+  //     })
 
-      if (signInAttempt?.error === null) redirect("/")
-      else {
-        setNeedsRegister(true)
-      }
-    } else {
-      console.log("register")
-      const register = await fetch("/api/auth/register", {
-        method: "POST",
-        body: JSON.stringify(data),
-      })
-      if (register.ok)
-        signIn("email", {
-          email: data.email,
-          callbackUrl: "/",
-        })
-    }
+  //     if (signInAttempt?.error === null) redirect("/")
+  //     else {
+  //       setNeedsRegister(true)
+  //     }
+  //   } else {
+  //     console.log("register")
+  //     const register = await fetch("/api/auth/register", {
+  //       method: "POST",
+  //       body: JSON.stringify(data),
+  //     })
+  //     if (register.ok)
+  //       signIn("email", {
+  //         email: data.email,
+  //         callbackUrl: "/",
+  //       })
+  //   }
+  // }
+
+  const onLogin: SubmitHandler<LoginType> = async (data) => {
+    console.log(data)
   }
 
   return (
@@ -58,21 +75,28 @@ export default function EmailSignOn() {
       className="flex flex-col gap-2 w-full"
       noValidate
     >
-      {needsRegister ? (
-        <TextInput
-          placeholder="Name"
-          type="text"
-          error={errors.name?.message ?? null}
-          {...register("name", { required: true })}
-        ></TextInput>
-      ) : null}
+      {/* <pre>{JSON.stringify(errors, null, 2)}</pre> */}
       <TextInput
-        placeholder="Email"
-        type="email"
-        error={errors.email?.message ?? null}
-        {...register("email", { required: true })}
+        placeholder="username"
+        type="text"
+        error={errors.username?.message ?? null}
+        {...register("username")}
       ></TextInput>
-      <DefaultButton className="justify-center">Create Account</DefaultButton>
+      <TextInput
+        placeholder="password"
+        type="password"
+        error={errors.password?.message ?? null}
+        {...register("password")}
+      ></TextInput>
+      <TextInput
+        placeholder="password (again)"
+        type="password"
+        error={errors.confirmPassword?.message ?? null}
+        {...register("confirmPassword")}
+      ></TextInput>
+      <DefaultButton type="submit" className="justify-center">
+        Create Account
+      </DefaultButton>
     </form>
   )
 }

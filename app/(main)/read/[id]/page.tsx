a310a58740af54b8119c425e39169c8973de7e76
Vurak <vurak.of.sulcan@gmail.com>
2024-06-27 16:31:55 +0700

    improve read/id layout

--- app/(main)/read/[id]/page.tsx
+++ app/(main)/read/[id]/page.tsx
@@ -1,6 +1,7 @@
 import EditorRead from "@/components/editor/editor-read"
 import { auth } from "@/lib/auth"
 import prisma from "@/lib/prisma"
+import { CSSProperties } from "react"
 
 export default async function Read({ params }: { params: { id: string } }) {
   const session = await auth()
@@ -15,12 +16,10 @@ export default async function Read({ params }: { params: { id: string } }) {
 
   if (tile)
     return (
-      <div className="relative whitespace-pre-wrap w-full h-full md:w-2/3 lg:w-1/2">
-        <div className="mb-2 flex justify-between">
-          <span>
-            <h1 className="font-bold">{tile.title}</h1>
-          </span>
-          <div className="absolute flex flex-row justify-between items-start top-0 right-0">
+      <div className="relative md:z-50 whitespace-pre-wrap w-full h-full pt-24 mx-5 sm:mx-20 md:mx-0 md:w-2/3 lg:w-1/2">
+        <div className="my-2 flex justify-between">
+          <p className="font-bold w-full">{tile.title}</p>
+          <div className="absolute flex flex-row justify-between items-start top-24 right-0">
             {/* {tile.id === session.data?.user.id ? ( */}
             <div className="mt-2 flex flex-col">
               <a className="w-16 underline" href={`/write/${tile.id}`}>

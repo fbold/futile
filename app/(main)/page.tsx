e3cdc8508d1cbdce6d0c6e321e15ef89d2e0eb26
vurak <vurak.of.sulcan@gmail.com>
2024-08-01 17:07:51 +0200

    styling adjustments

--- app/(main)/page.tsx
+++ app/(main)/page.tsx
@@ -9,13 +9,13 @@ export default function Home() {
           <br />
           <br />
           this is a space for writing for yourself and the{" "}
-          <span className="text-black font-semibold">void</span> / you can{" "}
+          <span className="text-white font-semibold">void</span> / you can{" "}
           <span className="text-yw">write</span> for any categories you choose /
           you can manage your categories by navigating to{" "}
           <span className="text-gn">.me</span> / you can{" "}
           <span className="text-rd">read</span> your documents / each document
           can be public or private / any document can be sent to the{" "}
-          <span className="text-black font-semibold">void</span> / you can{" "}
+          <span className="text-white font-semibold">void</span> / you can{" "}
           <span className="text-gn">visit</span> other futile users if you know
           their username
         </p>

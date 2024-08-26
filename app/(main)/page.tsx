5c838ca82225975a15210803534b1172b1c16f74
Vurak <vurak.of.sulcan@gmail.com>
2024-08-26 09:28:16 +0200

    adjustments to main page and linking to man

--- app/(main)/page.tsx
+++ app/(main)/page.tsx
@@ -1,9 +1,11 @@
 "use client"
 
+import Link from "next/link"
+
 export default function Home() {
   return (
     <>
-      <div className="w-full h-full flex items-center justify-center">
+      <div className="w-full h-full flex flex-col items-center justify-center">
         <p className="mx-6 sm:mx-0 sm:w-1/2 xl:w-1/5 text-justify whitespace-pre-wrap">
           welcome to <span className="underline">futile.me</span>
           <br />
@@ -11,16 +13,21 @@ export default function Home() {
           this is a space for writing for yourself and the{" "}
           <span className="text-white font-semibold">void</span> / you can{" "}
           <span className="text-yw">write</span> for any categories you choose /
-          you can manage your categories by navigating to{" "}
-          <span className="text-gn">.me</span> / you can{" "}
-          <span className="text-rd">read</span> your documents / any document
-          can be sent to the{" "}
+          you can <span className="text-rd">read</span> your documents / any
+          document can be sent to the{" "}
           <span className="text-white font-semibold">void</span> / documents in
           the <span className="text-white font-semibold">void</span> are visible
           to any user / there exists no way to link documents in the{" "}
           <span className="text-white font-semibold">void</span> to users / you
-          can <span className="text-gn">visit</span> other users (in
-          development)
+          can <span className="text-gn">visit</span> other users <br />
+          <br />
+        </p>
+        <p className="text-center">
+          read{" "}
+          <Link href="/meta/man" className="underline">
+            the manual
+          </Link>{" "}
+          for help
         </p>
       </div>
     </>

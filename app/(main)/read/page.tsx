c6947f41a3d55c28fb2e0cef913c5ba65500f3c7
vurak <vurak.of.sulcan@gmail.com>
2024-09-24 00:43:12 +0200

    layout adjustments to fix scroll issues

--- app/(main)/read/page.tsx
+++ app/(main)/read/page.tsx
@@ -22,7 +22,7 @@ export default async function Read({
   })
 
   return (
-    <div className="w-full h-full gap-2 overflow-scroll">
+    <div className="w-full h-full gap-2">
       <div className="flex flex-col my-12 px-4 w-full md:w-2/3 lg:w-3/5 gap-4 relative left-1/2 -translate-x-1/2">
         <TileList initialTiles={initialTiles} key={searchParams.c} />
       </div>

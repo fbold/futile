7ae418fd0ee40b084e529dc59a2d9a34820bbcfa
fbold <vurak.of.sulcan@gmail.com>
2024-11-24 23:21:22 +0100

    fix overflow on tiles after title changes

--- components/read/tile.tsx
+++ components/read/tile.tsx
@@ -1,6 +1,4 @@
 "use client"
-import { defaults } from "@/components/editor/editor-defaults"
-import { EditorContent, useEditor } from "@tiptap/react"
 import clsx from "clsx"
 import Link from "next/link"
 import { memo, useEffect, useRef, useState } from "react"
@@ -14,41 +12,26 @@ type Props = {
 const TilePreview = memo(({ tile, href }: Props) => {
   const containerRef = useRef<HTMLDivElement>(null)
   const divRef = useRef<HTMLDivElement>(null)
-  const titleRef = useRef<HTMLHeadingElement>(null)
+  //const titleRef = useRef<HTMLHeadingElement>(null)
   const [fadeText, setFadeText] = useState(false)
-
-  // const editor = useEditor({
-  //   ...defaults,
-  //   editable: false,
-  //   content: tile.content,
-  //   onUpdate() {
-  //     // Calculates if there is an overflow and shows fade if so
-  //     // The multiplier should be same as height in percent of tile div
-  //     if (
-  //       containerRef.current &&
-  //       divRef.current &&
-  //       window.visualViewport &&
-  //       titleRef.current
-  //     ) {
-  //       setFadeText(
-  //         divRef.current?.getBoundingClientRect().height +
-  //           titleRef.current?.getBoundingClientRect().height >=
-  //           containerRef.current?.getBoundingClientRect().height
-  //       )
-  //     }
-  //   },
-  // })
 
   useEffect(() => {
     if (
       containerRef.current &&
       divRef.current &&
-      window.visualViewport &&
-      titleRef.current
+      window.visualViewport
+      //      titleRef.current
     ) {
-      setFadeText(
+      console.log(
+        "----------------------------------",
         divRef.current?.getBoundingClientRect().height +
-          titleRef.current?.getBoundingClientRect().height >=
+          //         titleRef.current?.getBoundingClientRect().height,
+          "============================",
+        containerRef.current?.getBoundingClientRect().height,
+      )
+      setFadeText(
+        divRef.current?.getBoundingClientRect().height > //+
+          //titleRef.current?.getBoundingClientRect().height >=
           containerRef.current?.getBoundingClientRect().height,
       )
     }
@@ -57,10 +40,9 @@ const TilePreview = memo(({ tile, href }: Props) => {
   return (
     <div
       className={clsx(
-        "relative rounded-none block w-full flex-grow max-h-screen break-inside-avoid mb-1 cursor-pointer",
+        "relative flex flex-col w-full flex-grow max-h-screen break-inside-avoid mb-1 cursor-pointer",
         // !fadeText && "py-14"
       )}
-      ref={containerRef}
     >
       <Link href={href || `/read/${encodeURIComponent(tile.id ?? "")}`}>
         <VoidIcon inVoid={tile.inVoid}>{tile.title}</VoidIcon>
@@ -77,16 +59,18 @@ const TilePreview = memo(({ tile, href }: Props) => {
               .join(":")}  ${tile.createdAt.toLocaleDateString()}`
           : null}
       </p>
-      <div
-        ref={divRef}
-        className="overflow-hidden whitespace-pre-line space-y-2"
-        dangerouslySetInnerHTML={{ __html: tile.content! }}
-      >
-        {/* <EditorContent editor={editor} /> */}
+      <div className="overflow-y-hidden w-full h-auto" ref={containerRef}>
+        <div
+          ref={divRef}
+          className="overflow-hidden whitespace-pre-line space-y-2"
+          dangerouslySetInnerHTML={{ __html: tile.content! }}
+        >
+          {/* <EditorContent editor={editor} /> */}
+        </div>
       </div>
       {fadeText ? (
         <>
-          <span className="absolute rounded-b-lg -ml-2 w-full h-32 pointer-events-none bottom-0 bg-gradient-to-t from-sec from-5% to-transparent"></span>
+          <span className="absolute w-full h-32 pointer-events-none bottom-0 bg-gradient-to-t from-sec from-5% to-transparent"></span>
           <Link
             href={`/read/${encodeURIComponent(tile.id ?? "")}`}
             className="absolute bottom-2 left-1/2 -translate-x-1/2"
